2919a90372b241e671fc4f5fb6614a4335de0b9c
Kelly Kessler <ksquaredkelly@gmail.com>
2021-08-27 17:17:41 -0500

    complete phase 6

--- Program.cs
+++ Program.cs
@@ -23,34 +23,39 @@ namespace Guessing_Game
             Console.WriteLine("Your guess: ");
             // 3.1. Give the user four chances to guess the number.
             // 3.2. Continue to display the success or failure messages as in phase 2
-            for (int i = 5; i > 0; i--)
+            int guessesLeft = 3;
+
+            for (int i = 4; i > 0; i--)
             {
                 // 1.3 Take the user's guess as input and save it as a variable.
                 var guess = Console.ReadLine();
                 // 4.1. Display the number of the user's current guess in the prompt. For example, if the user has already guessed one time, the prommpt should say something like Your guess (2)>.
+                // 2. The prompt should display the number of guesses the user has left.
                 Console.WriteLine($"You guessed {guess}...");
                 // 2.2 No longer display the user's guess. They know what they guessed, right?
                 // 2.3 Compare the user's guess with the secret number. Display a success message if the guess is correct, otherwise display a failure message.
+                // 6.1. Inform the user if their guess was too high or too low, when they guess incorrectly.
                 int parsedGuess = int.Parse(guess);
+                guessesLeft = i - 1;
 
                 // 2. End the loop early if the user guesses the correct number.
                 if (parsedGuess == secretNumber)
                 {
-                    Console.WriteLine($"How'd you do that?! You won!");
+                    Console.WriteLine($"Dingdingding! How'd you do that?! Homer says you won!! (_8(|) ");
                     break;
                 }
                 else if (parsedGuess < secretNumber)
                 {
-                    Console.WriteLine("Too low! Try again - guesses left: {i}.");
+                    Console.WriteLine($"Rrrrrnnt! Too low! Guesses left: {guessesLeft}.");
                 }
                 else if (parsedGuess > secretNumber)
                 {
-                    Console.WriteLine("Too high! Try again.");
+                    Console.WriteLine($"Rrrrrnnt! Too high! Guesses left: {guessesLeft}.");
 
                 }
             }
-        }
 
+        }
     }
 
 }
@@ -60,13 +65,11 @@ namespace Guessing_Game
 // The program should be updated to...
 
 // 1. Use a random number to set the secret number between 1 and 100 instead of a hard-coded number.
-// 2. The prompt should display the number of guesses the user has left.
 
 // Phase 6
 
 // The program should be updated to...
 
-// 1. Inform the user if their guess was too high or too low, when they guess incorrectly.
 
 // Phase 7
 
