be75ee809fcbc235d785f0162049ab135d3dd3d7
Kelly Kessler <ksquaredkelly@gmail.com>
2021-08-27 20:25:30 -0500

    WIP phase 7

--- Program.cs
+++ Program.cs
@@ -1,56 +1,57 @@
-﻿// Write a console program in C# that invites the user to guess a number.
+﻿using System;
 
-// The program should be written in increments. Each phase will add a little more complexity.
-
-using System;
-
-namespace Guessing_Game
+namespace GuessingGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Phase 1
 
-            //* 2.1. Create a variable to contain the secret number. 
-            // 5.1. Use a random number to set the secret number between 1 and 100 instead of a hard-coded number.
             Random random = new Random();
             int secretNumber = random.Next(1, 101);
-            // The program should...
-            // 1.1 Display a message to the user asking them to guess the secret number.
+
             Console.WriteLine("Can you guess the secret number?");
+            Console.WriteLine("Before we start, tho, how much of a challenge do you like?");
+            Console.WriteLine(@"
+                - Easy - eight guesses (enter 1)
+                - Medium - six guesses (enter 2)
+                - Hard - four guesses (enter 3)
+                - IWannaPlayTillIWin!!! - infinity guesses (enter 4)"
+            );
 
-            // 1.2 Display a prompt for the user's guess.
-            Console.WriteLine("Your guess: ");
-            // 3.1. Give the user four chances to guess the number.
-            // 3.2. Continue to display the success or failure messages as in phase 2
-            int guessesLeft = 3;
+            var input = Console.ReadLine();
+            int userLevel = (int.Parse(input) - 1);
 
-            for (int i = 4; i > 0; i--)
+            int[] challengeLevels = { 8, 6, 4 };
+
+
+            // find a ternary? if userLevel[3], endless guesses, else original game/while loop
+            if
             {
-                // 1.3 Take the user's guess as input and save it as a variable.
-                var guess = Console.ReadLine();
-                // 4.1. Display the number of the user's current guess in the prompt. For example, if the user has already guessed one time, the prommpt should say something like Your guess (2)>.
-                // 2. The prompt should display the number of guesses the user has left.
-                Console.WriteLine($"You guessed {guess}...");
-                // 2.2 No longer display the user's guess. They know what they guessed, right?
-                // 2.3 Compare the user's guess with the secret number. Display a success message if the guess is correct, otherwise display a failure message.
-                // 6.1. Inform the user if their guess was too high or too low, when they guess incorrectly.
-                int parsedGuess = int.Parse(guess);
-                guessesLeft = i - 1;
+                int guessesLeft = challengeLevels[userLevel];
+            }
+            else if
 
-                // 2. End the loop early if the user guesses the correct number.
+            Console.WriteLine("First guess? ");
+            while (guessesLeft > 0)
+            {
+
+                var guess = Console.ReadLine();
+                int parsedGuess = int.Parse(guess);
+
                 if (parsedGuess == secretNumber)
                 {
-                    Console.WriteLine($"Dingdingding! How'd you do that?! Homer says you won!! (_8(|) ");
+                    Console.WriteLine($"Dingdingding! How'd you do that?! Homer Simpson says you won!! (_8(|) ");
                     break;
                 }
                 else if (parsedGuess < secretNumber)
                 {
+                    guessesLeft--;
                     Console.WriteLine($"Rrrrrnnt! Too low! Guesses left: {guessesLeft}.");
                 }
                 else if (parsedGuess > secretNumber)
                 {
+                    guessesLeft--;
                     Console.WriteLine($"Rrrrrnnt! Too high! Guesses left: {guessesLeft}.");
 
                 }
@@ -60,30 +61,6 @@ namespace Guessing_Game
     }
 
 }
-
-// Phase 5
-
-// The program should be updated to...
-
-
-// Phase 6
-
-// The program should be updated to...
-
-
-// Phase 7
-
-// The program should be updated to...
-
-// 1. Prompt the user for a difficulty level before they are prompted to guess the number.
-// 2. The difficulty level should determine how many guesses the user gets. The difficulty levels should be:
-// 	- Easy - this gives the user eight guesses.
-// 	- Medium - this gives the user six guesses.
-// 	- Hard - this gives the user four guesses.
-
-// Phase 8
-
-// The program should be updated to...
 
 // 1. Add a difficulty level of "Cheater" which will cause the program to continue prompting the user until they get the answer correct.
 //     }
